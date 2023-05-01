4d97d7c4fa41dc3ffb67ea1c7455ac7334a32570
Alexander Krasnov <krasnov.alx@gmail.com>
2023-05-01 20:57:44 +0200

    Reader working

---
+++ vsett_display_dual_vesc.lisp
@@ -0,0 +1,77 @@
+;;;UART configuration on COM-Port
+;;;Refer to https://github.com/aka13-404/IO-Hawk-Legacy-Info for protocol details
+(uart-start 1200)
+(define display-packet (array-create type-byte 16))
+(define packet-length 15)
+
+(define encoding-key-array [
+    0x5e 0x23 0x5c 0x21 0x2a 0x2f 0x28 0x2d 0x26 0x2b 0x24 0x29 0x52 0x57 0x50 0x55
+    0x4e 0x53 0x4c 0x51 0x5a 0x5f 0x58 0x5d 0x56 0x5b 0x54 0x59 0x02 0x07 0x00 0x05
+    0x3e 0x03 0x3c 0x01 0x0a 0x0f 0x08 0x0d 0x06 0x0b 0x04 0x09 0x32 0x37 0x30 0x35
+    0x2e 0x33 0x2c 0x31 0x3a 0x3f 0x38 0x3d 0x36 0x3b 0x34 0x39 0x62 0x67 0x60 0x65
+    0x1e 0x63 0x1c 0x61 0x6a 0x6f 0x68 0x6d 0x66 0x6b 0x64 0x69 0x12 0x17 0x10 0x15
+    0x0e 0x13 0x0c 0x11 0x1a 0x1f 0x18 0x1d 0x16 0x1b 0x14 0x19 0x42 0x47 0x40 0x45
+    0x7e 0x43 0x7c 0x41 0x4a 0x4f 0x48 0x4d 0x46 0x4b 0x44 0x49 0x72 0x77 0x70 0x75
+    0x6e 0x73 0x6c 0x71 0x7a 0x7f 0x78 0x7d 0x76 0x7b 0x74 0x79 0x22 0x27 0x20 0x25])
+
+
+
+
+;;;debug
+(define debug 0)
+
+(defun print-bytes (x) 
+    ;;If I have to write that again just to print a byte array I don't know what I am going to do
+    (progn
+        (setvar 'debug-print-packet "")
+        (loopfor each 0 (< each packet-length) (+ each 1)
+                (setvar 'debug-print-packet (str-merge debug-print-packet (str-from-n (bufget-u8 x each)) " "))
+        )
+        (print debug-print-packet)
+    )
+)
+;;;debug end
+
+;Calculate bitwise xor for all bytes
+(defun crc-calc (x)
+    (progn
+        (setvar 'crc 0)
+        (loopfor each 0 (< each (- packet-length 1) ) (+ each 1)
+            (setvar 'crc (bitwise-xor crc (bufget-u8 x each)))
+        )
+    )
+)
+        
+
+
+
+
+(defun reader ()
+    (loopwhile t
+        (progn
+            (uart-read-bytes display-packet packet-length 0)
+            (if (= debug 1) (print-bytes display-packet)); print received packet if debug on
+            (if (and (eq (bufget-u8 display-packet 0) 1)
+                     (eq (bufget-u8 display-packet 1) 3)
+                     (eq (bufget-u8 display-packet 14) (crc-calc display-packet))
+                );Byte0 = 1, Byte1 = 3, Checksum bitwise xor 0-13 byte
+                (progn ;Packet received, everything good, do stuff:
+                    ;(print "Packet received, everything good")
+                )
+                (uart-read-bytes display-packet 1 0) ;Else: skip 1 byte forward
+            )
+        )
+    )
+)
+            
+(defun writer ()
+    (loopwhile t
+        (progn
+            (yield 450000)
+        )
+    )
+)
+
+
+(spawn 150 reader)
+(spawn 150 writer) 
